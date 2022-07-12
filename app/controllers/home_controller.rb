1a75ac85a588758ee7ea15c617bec030d2a3379a
Teng Jian Ling <tengjianling98@gmail.com>
2022-07-12 16:41:44 +0800

    fix delete all button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+  def destroy_all
+    Team.destroy_all
+    Match.destroy_all
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # get "/create-registrations" => "registrations#create_registrations"
   get "/results" => "results#index"
 
-  match '/destroy_them_all', to: 'registrations_creators#destroy_them_all', via: :delete
+  # match '/destroy_them_all', to: 'registrations_creators#destroy_them_all', via: :delete
+  match '/destroy_all', to: 'home#destroy_all', via: :delete
+  match '/destroy_all', to: 'home#destroy_all', via: :get
 
   # Defines the root path route ("/")
   root "home#index"
